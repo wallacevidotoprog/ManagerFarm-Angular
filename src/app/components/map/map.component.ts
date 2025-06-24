e0b62a290f68d4f52bd7bf0ba68243bb864a93bf
WallaceVidoto <wallacevidoto.tech@gmail.com>
2025-06-24 19:09:45 -0300

    remodelacao do map

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -3,80 +3,210 @@ import {
   AfterViewInit,
   Component,
   ElementRef,
-  Output,
+  output,
   ViewChild,
 } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { area, polygon } from '@turf/turf';
 import * as L from 'leaflet';
 import { MapMarkers, MapPoint } from '../../@types/map-point.types';
-import { MatSelectModule } from '@angular/material/select';
-import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-map',
   template: `
     <div
-      style="margin-bottom: 10px; display: flex; gap: 10px; flex-wrap: wrap;justify-content: center;    margin: 10px;"
+      style="
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    gap: 16px;
+    margin: 16px auto;
+    max-width: 100%;
+  "
     >
-      <button mat-raised-button color="primary" (click)="toggleMarking()">
+    <div><span>Área: {{ hectares }} </span></div>
+      <button mat-fab extended color="primary" (click)="toggleMarking()">
+        <mat-icon>edit_location</mat-icon>
         {{ isMarkingActive ? 'Desativar marcação' : 'Ativar marcação' }}
       </button>
 
-      <button mat-raised-button color="warn" (click)="clearPoints()">
+      <button mat-fab extended color="warn" (click)="clearPoints()">
+        <mat-icon>delete_forever</mat-icon>
         Limpar pontos
       </button>
-      <mat-form-field appearance="fill">
-        <mat-label>Estilo do Mapa</mat-label>
-        <mat-select
-          [(value)]="selectedStyle"
-          (selectionChange)="changeMapStyle()"
-        >
-          <mat-option *ngFor="let key of mapStyleKeys" [value]="key">
+
+      <div
+        class="map-style-select"
+        (click)="toggleDropdown()"
+        tabindex="0"
+        (blur)="dropdownOpen = false"
+      >
+        <span class="select-button">
+          <span class="material-icons option-icon">
+            {{ mapStyles[selectedStyle].icon }}
+          </span>
+          {{ mapStyles[selectedStyle].name }}
+        </span>
+        <span class="material-icons arrow-icon">&#9662;</span>
+
+        <ul class="options-dropdown" *ngIf="dropdownOpen">
+          <li
+            *ngFor="let key of mapStyleKeys"
+            (click)="selectStyle(key)"
+            [class.selected]="key === selectedStyle"
+          >
+            <span class="material-icons option-icon">
+              {{ mapStyles[key].icon }}
+            </span>
             {{ mapStyles[key].name }}
-          </mat-option>
-        </mat-select>
-      </mat-form-field>
+          </li>
+        </ul>
+      </div>
     </div>
 
     <div
       #map
       [ngStyle]="{ cursor: isMarkingActive ? 'crosshair' : 'grab' }"
-      style="height: 400px; margin: 20px 0; border: 1px solid #ccc; border-radius: 8px;"
+      style="height: 600px; margin: 20px 0; border: 1px solid #ccc; border-radius: 8px;"
     ></div>
   `,
-  styles: ``,
+  styles: `.map-style-select {
+  position: relative;
+  display: inline-block;
+  user-select: none;
+  outline: none;
+  width: 220px;
+  font-family: Roboto, Arial, sans-serif;
+}
+
+.select-button {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 8px;
+  background-color: #d7e3ff;
+  color: #00458f;
+  padding: 8px 16px;
+  border-radius: 16px;
+  cursor: pointer;
+  font-size: 16px;
+  font-family: Roboto, Arial, sans-serif;
+  font-size: 0.875rem;
+  font-weight: 500;
+  box-shadow: 0px 3px 5px -1px rgba(0, 0, 0, 0.2), 0px 6px 10px 0px rgba(0, 0, 0, 0.14), 0px 1px 18px 0px rgba(0, 0, 0, 0.12);
+  height: 38px;
+  user-select: none;
+}
+
+.select-button:hover {
+  background-color:rgb(198, 214, 252);
+  box-shadow:0px 5px 5px -3px rgba(0, 0, 0, 0.2), 0px 8px 10px 1px rgba(0, 0, 0, 0.14), 0px 3px 14px 2px rgba(0, 0, 0, 0.12);;
+}
+
+.option-icon {
+  font-size: 22px;
+  line-height: 1;
+}
+
+.arrow-icon {
+  position: absolute;
+  right: 12px;
+  top: 50%;
+  transform: translateY(-50%);
+  font-size: 16px;
+  pointer-events: none;
+  color: #00458f;
+}
+
+.options-dropdown {
+  position: absolute;
+  top: 110%;
+  left: 0;
+  width: 100%;
+  background-color: white;
+  box-shadow: 0 4px 8px rgba(0,0,0,0.15);
+  border-radius: 8px;
+  margin: 4px 0 0 0;
+  padding: 4px 0;
+  max-height: 200px;
+  overflow-y: auto;
+  z-index: 1000;
+}
+
+.options-dropdown li {
+  padding: 10px 16px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  font-weight: 500;
+  color: #333;
+  transition: background-color 0.2s ease;
+  user-select: none;
+}
+
+.options-dropdown li.selected,
+.options-dropdown li:hover {
+  background-color: #d7e3ff;
+  color: #00458f;
+}
+
+.options-dropdown li .option-icon {
+  font-size: 20px;
+}
+`,
   standalone: true,
-  imports: [MatButtonModule, CommonModule,MatButtonModule, MatSelectModule, MatFormFieldModule],
+  imports: [
+    MatButtonModule,
+    CommonModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatDividerModule,
+    MatIconModule,
+  ],
 })
 export class MapComponent implements AfterViewInit {
   @ViewChild('map', { static: true }) mapElement!: ElementRef<HTMLDivElement>;
   protected mapPoints: MapPoint[] = [];
+  public returnPoints = output<MapPoint[]>();
   private pointIdCounter = 0;
   private map!: L.Map;
   private markers: MapMarkers[] = [];
   private polyline: L.Polyline | null = null;
   protected isMarkingActive = false;
   protected selectedStyle: keyof typeof this.mapStyles = 'default';
+  dropdownOpen = false;
+  hectares: string = '0 ha' ;
 
-  
   protected mapStyles = {
     default: {
       name: 'Padrão',
+      icon: 'map',
       url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
       attribution: '&copy; OpenStreetMap contributors',
     },
     satellite: {
       name: 'Satélite (Esri)',
+      icon: 'satellite',
       url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
       attribution: 'Tiles © Esri, i-cubed, USDA, USGS, AEX, GeoEye, etc.',
     },
     dark: {
       name: 'Escuro (CartoDB)',
+      icon: 'dark_mode',
       url: 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
       attribution: '&copy; CartoDB',
     },
-  } as const;;
-  protected mapStyleKeys = Object.keys(this.mapStyles) as (keyof typeof this.mapStyles)[];
+  } as const;
+  protected mapStyleKeys = Object.keys(
+    this.mapStyles
+  ) as (keyof typeof this.mapStyles)[];
 
   private tileLayer!: L.TileLayer;
 
@@ -102,23 +232,23 @@ export class MapComponent implements AfterViewInit {
       13
     );
 
-    // L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    //   attribution: '&copy; OpenStreetMap contributors',
-    // }).addTo(this.map);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; OpenStreetMap contributors',
+    }).addTo(this.map);
     // L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
     //   attribution: '&copy; CartoDB'
     // }).addTo(this.map);
     // L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
     // attribution: '&copy; CartoDB'
     // }).addTo(this.map);
-    L.tileLayer(
-      'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
-      {
-        attribution:
-          'Tiles © Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, etc.',
-        maxZoom: 19,
-      }
-    ).addTo(this.map);
+    // L.tileLayer(
+    //   'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+    //   {
+    //     attribution:
+    //       'Tiles © Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, etc.',
+    //     maxZoom: 19,
+    //   }
+    // ).addTo(this.map);
 
     L.marker([mylocation.lat, mylocation.lng], {
       icon: this.homeIcon,
@@ -170,8 +300,12 @@ export class MapComponent implements AfterViewInit {
       if (p) {
         p.lat = pos.lat;
         p.lng = pos.lng;
+        marker.bindTooltip(
+          `Lat: ${pos.lat.toFixed(4)}, Lng: ${pos.lng.toFixed(4)}`
+        );
         this.drawPolyline();
       }
+      this.onEventEmitPoints();
     });
 
     marker.on('click', () => {
@@ -182,6 +316,7 @@ export class MapComponent implements AfterViewInit {
 
     this.markers.push({ id, marker });
     this.drawPolyline();
+    this.onEventEmitPoints();
   }
 
   removePoint(id: number) {
@@ -192,6 +327,7 @@ export class MapComponent implements AfterViewInit {
     }
     this.mapPoints = this.mapPoints.filter((p) => p.id !== id);
     this.drawPolyline();
+    this.onEventEmitPoints();
   }
 
   drawPolyline() {
@@ -217,6 +353,7 @@ export class MapComponent implements AfterViewInit {
       this.map.removeLayer(this.polyline);
       this.polyline = null;
     }
+    this.onEventEmitPoints();
   }
 
   toggleMarking() {
@@ -224,14 +361,53 @@ export class MapComponent implements AfterViewInit {
   }
 
   changeMapStyle() {
-  if (this.tileLayer) {
-    this.map.removeLayer(this.tileLayer);
+    if (this.tileLayer) {
+      this.map.removeLayer(this.tileLayer);
+    }
+
+    const style = this.mapStyles[this.selectedStyle];
+
+    this.tileLayer = L.tileLayer(style.url, {
+      attribution: style.attribution,
+    }).addTo(this.map);
   }
 
-  const style = this.mapStyles[this.selectedStyle];
+  toggleDropdown() {
+    this.dropdownOpen = !this.dropdownOpen;
+  }
 
-  this.tileLayer = L.tileLayer(style.url, {
-    attribution: style.attribution,
-  }).addTo(this.map);
-}
+  selectStyle(key: keyof typeof this.mapStyles) {
+    this.selectedStyle = key;
+    this.dropdownOpen = false;
+    this.changeMapStyle();
+    this.toggleDropdown();
+  }
+
+  onEventEmitPoints() {
+  const hectares = this.getPolygonAreaInHectares();
+    this.hectares = hectares.toFixed(2);
+  }
+
+  public invalidateMapSize(): void {
+    if (this.map) {
+      setTimeout(() => this.map.invalidateSize(), 100); // pequeno delay para garantir renderização
+    }
+  }
+
+  getPolygonAreaInHectares(): number {
+    if (this.mapPoints.length < 3) return 0;
+
+    // Converte para GeoJSON format
+    const coordinates = [
+      [
+        ...this.mapPoints.map((p) => [p.lng, p.lat]),
+        [this.mapPoints[0].lng, this.mapPoints[0].lat],
+      ], // fecha o polígono
+    ];
+
+    const poly = polygon(coordinates);
+    const areaInSqMeters = area(poly);
+
+    return areaInSqMeters / 10000; // metros quadrados → hectares
+  }
 }
--- src/app/pages/property-form/property-form.component.ts
+++ src/app/pages/property-form/property-form.component.ts
@@ -1,149 +1,99 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, inject, OnInit, ViewChild } from '@angular/core';
 import {
-  AfterViewInit,
-  Component,
-  ElementRef,
-  ViewChild,
-  inject,
-} from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import * as L from 'leaflet';
-import { MapComponent } from "../../components/map/map.component";
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTabChangeEvent, MatTabsModule } from '@angular/material/tabs';
+import { MapPoint } from '../../@types/map-point.types';
+import { MapComponent } from '../../components/map/map.component';
 
-interface MapPoint {
-  id: number;
-  lat: number;
-  lng: number;
+enum PropertyStatus {
+  NONE = 'NONE',
+  AVAILABLE = 'DISPONÍVEL',
+  SOLD = 'VENDIDA',
+  RENTED = 'ALUGADA',
 }
-
 @Component({
   selector: 'app-property-form',
   standalone: true,
   templateUrl: './property-form.component.html',
   styleUrls: ['./property-form.component.scss'],
-  imports: [CommonModule, FormsModule, HttpClientModule, MapComponent,MapComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    HttpClientModule,
+    MapComponent,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatTabsModule,
+    MatButtonToggleModule,
+  ],
 })
-export class PropertyFormComponent implements AfterViewInit {
-  name = '';
-  location = '';
-  size = 0;
-  userId = '';
+export class PropertyFormComponent implements OnInit {
+  @ViewChild('mapComp') mapComponent!: MapComponent;
+
   mapPoints: MapPoint[] = [];
 
   private http: HttpClient = inject(HttpClient);
-  private pointIdCounter = 0;
+  propertyForm!: FormGroup;
+  statusOptions = Object.values(PropertyStatus);
 
-  @ViewChild('map', { static: true }) mapElement!: ElementRef<HTMLDivElement>;
-  private map!: L.Map;
+  constructor(private fb: FormBuilder) {}
 
-  private markers: { id: number; marker: L.Marker }[] = [];
-  private polyline: L.Polyline | null = null;
-
-  isMarkingActive = false;
-
-  customIcon = L.icon({
-    iconUrl: 'assets/pin.png',
-    iconSize: [32, 32],
-    iconAnchor: [16, 32],
-    popupAnchor: [0, -32],
-  });
-
-  ngAfterViewInit() {
-    this.map = L.map(this.mapElement.nativeElement).setView([-22.9, -43.2], 13);
-
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; OpenStreetMap contributors',
-    }).addTo(this.map);
-
-    this.map.on('click', (e: any) => {
-      if (!this.isMarkingActive) return;
-
-      const { lat, lng } = e.latlng;
-      this.addPoint(lat, lng);
+  ngOnInit(): void {
+    this.propertyForm = this.fb.group({
+      name: ['', Validators.required],
+      location: ['', Validators.required],
+      size: [null, Validators.required],
+      mapPoints: [this.mapPoints, Validators.required], // Pode ser melhorado com textarea ou JSON editor
+      ownerId: ['', Validators.required],
+      userId: ['', Validators.required],
+      status: [PropertyStatus.NONE, Validators.required],
+      description: [''],
     });
   }
 
-  addPoint(lat: number, lng: number) {
-    const id = this.pointIdCounter++;
-    const point: MapPoint = { id, lat, lng };
-    this.mapPoints.push(point);
-
-    const marker = L.marker([lat, lng], {
-      icon: this.customIcon,
-      draggable: true,
-    }).addTo(this.map);
-
-    marker.on('dragend', () => {
-      const pos = marker.getLatLng();
-      const p = this.mapPoints.find((p) => p.id === id);
-      if (p) {
-        p.lat = pos.lat;
-        p.lng = pos.lng;
-        this.drawPolyline();
-      }
-    });
-
-    marker.on('click', () => {
-      this.removePoint(id);
-    });
-
-    this.markers.push({ id, marker });
-    this.drawPolyline();
-  }
-
-  removePoint(id: number) {
-    const markerObj = this.markers.find((m) => m.id === id);
-    if (markerObj) {
-      this.map.removeLayer(markerObj.marker);
-      this.markers = this.markers.filter((m) => m.id !== id);
+  onSubmit() {
+    if (this.propertyForm.valid) {
+      console.log('Form data:', this.propertyForm.value);
     }
-    this.mapPoints = this.mapPoints.filter((p) => p.id !== id);
-    this.drawPolyline();
-  }
+    return;
 
-  drawPolyline() {
-    if (this.polyline) {
-      this.map.removeLayer(this.polyline);
-    }
-
-    if (this.mapPoints.length < 2) {
-      return;
-    }
-
-    const latlngs: L.LatLngTuple[] = this.mapPoints.map((p) => [p.lat, p.lng]);
-    latlngs.push([this.mapPoints[0].lat, this.mapPoints[0].lng]); // fecha o polígono
-
-    this.polyline = L.polyline(latlngs, { color: 'blue' }).addTo(this.map);
-  }
-
-  clearPoints() {
-    this.markers.forEach((m) => this.map.removeLayer(m.marker));
-    this.markers = [];
-    this.mapPoints = [];
-    if (this.polyline) {
-      this.map.removeLayer(this.polyline);
-      this.polyline = null;
-    }
-  }
-
-  toggleMarking() {
-    this.isMarkingActive = !this.isMarkingActive;
-  }
-
-  submit() {
     this.http
       .post('http://localhost:3000/property', {
         name: 'this.name',
         location: 'this.location',
         size: 5,
         userId: '095abb78-66e2-44d1-9101-8c136b492f34',
-        ownerId:'095abb78-66e2-44d1-9101-8c136b492f34',
+        ownerId: '095abb78-66e2-44d1-9101-8c136b492f34',
         mapPoints: this.mapPoints,
       })
       .subscribe({
         next: () => alert('Propriedade salva!'),
         error: () => alert('Erro ao salvar!'),
       });
+  }
+
+  onMapPointsReturn($event: MapPoint[]) {
+    this.mapPoints = $event;
+  }
+  onTabChange(event: any): void {
+    if (event.index === 2) { 
+      this.mapComponent.invalidateMapSize();
+    }
   }
 }
